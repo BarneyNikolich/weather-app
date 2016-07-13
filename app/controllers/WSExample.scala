e9fc63ab73e98086507a24daeb8c4c51b636447e
Barney Nikolich <barney.nikolich@gmail.com>
2016-07-14 00:06:34 +0100

    Successful GET request to MET Office API

--- app/controllers/WSExample.scala
+++ app/controllers/WSExample.scala
@@ -2,27 +2,33 @@ package controllers
 
 
 import javax.inject.Inject
-import scala.concurrent.Future
-import scala.concurrent.duration._
 
-import play.api.mvc._
 import play.api.libs.ws._
-import play.api.http.HttpEntity
+import play.api.mvc.{Result, Action, Controller}
 
-import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl._
-import akka.util.ByteString
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
-import scala.concurrent.ExecutionContext
 
-class WSExample @Inject() (ws: WSClient) extends Controller {
+class WSExample @Inject() (ws: WSClient) extends Controller() {
 
   lazy val API_KEY = "baff2146-ee96-429f-b077-efc1429e763a"
 
+  def index = Action {
+    Ok(views.html.index())
+  }
 
-  //Calling all locations
-  val testUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/3840?res=3hourly&key="+API_KEY
-  val request: WSRequest = ws.url(testUrl)
+
+  def showGetResult = Action.async {
+
+    val testUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/3840?res=3hourly&key="+API_KEY
+
+    val result: Future[Result] = ws.url(testUrl).get() map {
+      response =>
+        Ok(response.body)
+    }
+    result
+  }
+
 
 }
